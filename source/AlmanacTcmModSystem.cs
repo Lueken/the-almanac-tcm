1fa9942517b3cd901f4097ad67f312c12370e9f8
Jeffrey <jdlueken@gmail.com>
2026-07-27 12:57:31 -0700

    0.4.6: smithing breadcrumb names its ingredients in bold (tester feedback)

--- source/AlmanacTcmModSystem.cs
+++ source/AlmanacTcmModSystem.cs
@@ -8,7 +8,7 @@ using Vintagestory.API.Server;
 [assembly: ModInfo("The Almanac: Trades, Callings & Mastery", "almanactcm",
     Authors = new string[] { "Venah" },
     Description = "Identity-first trade progression for the modded world.",
-    Version = "0.4.5")]
+    Version = "0.4.6")]
 
 namespace AlmanacTcm;
 
